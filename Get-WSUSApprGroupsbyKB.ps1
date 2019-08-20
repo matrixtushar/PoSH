d8f5191ed4f3ee4ea2d72da399d4cf8f10cbab11
Tushar SINGH <38909335+matrixtushar@users.noreply.github.com>
2019-08-20 21:34:41 +0800

    Update and rename Get-WSUSApprGroupsbyKB to Get-WSUSApprGroupsbyKB.ps1

--- Get-WSUSApprGroupsbyKB.ps1
+++ Get-WSUSApprGroupsbyKB.ps1
@@ -2,7 +2,7 @@
 # Script Author: Tushar Singh [matrixtushar@gmail.com]
 # Script Purpose: Gets the Computer Target Groups in WSUS where the specified KB has been approved
 # Script Business Usage: The script can be used to monitor whether selected KBs have been approved on desired target groups
-
+# ----------------------
 
 [reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration") | Out-Null
 $wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::getUpdateServer('WSUS_Server_Name',$false)
